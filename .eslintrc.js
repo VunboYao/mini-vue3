6ad7ae5dde4329f6a7e29c92ca6ce212d33c00a7
VunboYao <vunbo@outlook.com>
2022-03-30 16:59:48 +0800

    feat(vue3): init Project

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'amd': true,
+    'node': true
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    quotes: [2, 'single'],
+    semi: [2, 'never']
+  }
+}
