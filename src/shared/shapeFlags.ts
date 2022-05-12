97497c128dfd3a138bdd57ea1b8443ce4216e47b
VunboYao <vunbo@outlook.com>
2022-05-12 14:40:54 +0800

    perf:位运算来优化判断条件

--- src/shared/shapeFlags.ts
+++ src/shared/shapeFlags.ts
@@ -1,6 +1,6 @@
 export const enum ShapeFlags {
-  ELEMENT = 1, // 0001
-  STATEFUL_COMPONENT = 1 << 1, // 0010
-  TEXT_CHILDREN = 1 << 2, // 0100
-  ARRAY_CHILDREN = 1 << 3, // 1000
+  ELEMENT = 1, // 0001 element元素
+  STATEFUL_COMPONENT = 1 << 1, // 0010 组件
+  TEXT_CHILDREN = 1 << 2, // 0100 文本
+  ARRAY_CHILDREN = 1 << 3, // 1000 数组
 }
