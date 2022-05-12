22ff6430e39aea462a7adcd04d8abe0bf1b6d074
VunboYao <vunbo@outlook.com>
2022-05-12 14:56:34 +0800

    feat(event): 事件注册

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -8,12 +8,20 @@ export default {
     return h('div', {
       id: 'root',
       class: 'blue',
+      onClick() {
+        // eslint-disable-next-line no-console
+        console.log('click')
+      },
+      onMousedown() {
+        // eslint-disable-next-line no-console
+        console.log('mousedown')
+      },
     },
-    // `Hi, ${this.msg}`, // string
-    [
-      h('span', { class: 'red' }, 'Hello'),
-      h('span', { class: 'blue' }, ' Vue3'),
-    ],
+      `Hi, ${this.msg}`, // string
+      // [
+      //   h('span', { class: 'red' }, 'Hello'),
+      //   h('span', { class: 'blue' }, ' Vue3'),
+      // ],
     )
   },
   setup() {
